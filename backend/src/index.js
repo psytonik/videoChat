948959ae771b8627a2823ea47d6c0c72b363c21b
psytonik <psytonik@icloud.com>
2025-12-18 10:15:21 +0200

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,7 @@ if(process.env.NODE_ENV === 'production') {
     console.log(express.static(path.join(__dirname,'../frontend/dist')), 'HERE IM')
     app.use(express.static(path.join(__dirname,'../frontend/dist')));
 
-    app.get('/', (req, res) => {
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.join(__dirname,'../frontend', '/dist', '/index.html'));
     })
 }
